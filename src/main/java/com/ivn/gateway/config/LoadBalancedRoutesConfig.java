a4bf6ad97ed4b73e8f0c5bb273ce2af6e367470c
Ivan Ivanyshyn <ivan.ivanyshyn@cgm.com>
2022-02-06 20:41:27 +0200

    adding Resilience4J Circuit Breaker, closes #5

--- src/main/java/com/ivn/gateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/ivn/gateway/config/LoadBalancedRoutesConfig.java
@@ -8,17 +8,22 @@ import org.springframework.context.annotation.Profile;
 
 @Profile("local-discovery")
 @Configuration
-public class LoadBalancedRoutes {
+public class LoadBalancedRoutesConfig {
 
     @Bean
-    public RouteLocator loadBalanceRoutes(RouteLocatorBuilder builder){
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("beer-service", r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("lb://beer-service"))
                 .route("order-service", r -> r.path("/api/v1/customers/**")
                         .uri("lb://order-service"))
                 .route("inventory-service", r -> r.path("/api/v1/beer/*/inventory")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                .setFallbackUri("forward:/inventory-failover")
+                                .setRouteId("inv-failover")))
                         .uri("lb://inventory-service"))
+                .route("inventory-failover-service", r -> r.path("/inventory-failover/**")
+                        .uri("lb://inventory-failover"))
                 .build();
     }
 }
